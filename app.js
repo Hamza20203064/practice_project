734e80b53a0d49503299d0f977f25876d0fc90be
HAMZA HAMAD <hamzaahmadhashim9@gmail.com>
2025-03-23 19:38:17 +0000

    DB Connected

--- app.js
+++ app.js
@@ -2,11 +2,19 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const ejs = require('ejs')
+const mongoose = require('mongoose')
 // const { env } = require('process')
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 app.use(express.static(path.join(__dirname, 'public')))
-
+mongoose.connect('mongodb://127.0.0.1:27017/dryFruites')
+.then(()=>{
+    console.log("DB connected sucessfully");
+})
+.catch((error)=>{
+    console.error();
+    
+})
 
 const PORT = 8080
 app.listen(PORT, ()=>{
