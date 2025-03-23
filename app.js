94440b8e5fe0e5f90c000ff3b363b5c8312ce6bb
HAMZA HAMAD <hamzaahmadhashim9@gmail.com>
2025-03-23 20:55:58 +0530

    adding schemas

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const ejs = require('ejs')
-const { env } = require('process')
-
+// const { env } = require('process')
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+app.use(express.static(path.join(__dirname, 'public')))
 
 
 const PORT = 8080
---
+++ models/Project.js
@@ -0,0 +1,14 @@
+const mongoos = require('mongoose')
+ 
+const products{
+// name:
+// discription:
+// quality:
+// rating:
+// quantity:
+// expiry date:
+// production date:
+// price:
+// pictures:
+
+}
